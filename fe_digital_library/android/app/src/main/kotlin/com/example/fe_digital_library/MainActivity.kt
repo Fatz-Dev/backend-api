feb9e81ffee9a23050f687186232c4ddc864b3a1
Fatz-Dev <fatazikrillah007@gmail.com>
2026-06-19 23:11:58 +0700

    Fix: bersihkan cache gitignore

--- fe_digital_library/android/app/src/main/kotlin/com/example/fe_digital_library/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fe_digital_library
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- fe_digital_library/lib/screens/tambahan.md
+++
@@ -1,2 +0,0 @@
-
-- tambahan profile serta menu buat jadi navigation bottom pakai convex
